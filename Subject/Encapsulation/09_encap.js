26357178b1f47b39da348d4f439c0db6aae66ec5
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-04 15:42:11 +0300

    Update 09_encap.js

--- Subject/Encapsulation/09_encap.js
+++ Subject/Encapsulation/09_encap.js
@@ -1,1 +1,48 @@
+---------------------------------------------------------------------------------------------  
+⋙ ❍ Инкапсуляция (сделал все поля класса публичными и не создал конструктор)):
+---
+9. Полиморфизм с инкапсуляцией
+⌛ Задача: Создайте класс `Shape` и наследников `Circle` и `Square`.
+🎯 Решение:
+```javascript
+class Shape {
+  #type;
 
+  constructor(type) {
+    this.#type = type;
+  }
+
+  getType() {
+    return this.#type;
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    super("Circle");
+    this.radius = radius;
+  }
+
+  getArea() {
+    return Math.PI * this.radius ** 2;
+  }
+}
+
+class Square extends Shape {
+  constructor(side) {
+    super("Square");
+    this.side = side;
+  }
+
+  getArea() {
+    return this.side ** 2;
+  }
+}
+
+const shapes = [new Circle(5), new Square(4)];
+
+shapes.forEach((shape) =>
+  console.log(`${shape.getType()} Area: ${shape.getArea()}`)
+);
+```
+--------------------------------------------------------------------------------------------- 
