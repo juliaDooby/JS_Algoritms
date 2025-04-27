3d0860430e16858218aa8fdad250810eadb12711
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-28 00:26:30 +0300

    Create 03_sList.js

---
+++ Subject/Singly_linked-list/03_sList.js
@@ -0,0 +1,58 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ Односвязный список:
+---
+3️⃣ Поиск элемента в списке
+⌛ Задача: Написать метод для поиска элемента в списке.
+🎯 Решение:
+```js
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  find(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (current.data === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  print() {
+    let current = this.head;
+    let result = '';
+    while (current !== null) {
+      result += current.data + ' -> ';
+      current = current.next;
+    }
+    console.log(result.slice(0, -4));
+  }
+}
+
+const list = new SinglyLinkedList();
+list.add(10);
+list.add(20);
+list.add(30);
+list.print(); // 10 -> 20 -> 30
+console.log(list.find(20)); // true
+console.log(list.find(40)); // false
+```
+Объяснение:
+- Метод `find()` ищет элемент в списке, перебирая его узлы.
+---------------------------------------------------------------------------------------------
